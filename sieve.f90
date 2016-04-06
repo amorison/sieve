cff1b6d1b66ad090f3175e212ab6a9b727ef1361
Adrien Morison <adrien.morison@gmail.com>
2016-04-06 03:20:58 +0200

    Use bit to store primality instead of logical
    32 times less memory is used, execution time are
    similar.

--- sieve.f90
+++ sieve.f90
@@ -2,30 +2,36 @@ program sieve
     use iso_fortran_env
     implicit none
 
-    integer(kind=int64), parameter:: i_max = 1000
-    integer(kind=int64), parameter:: n_max = 2*i_max+1
+    integer(kind=int64), parameter:: i_max = 7812500
+    integer(kind=int64), parameter:: b_max = 64*i_max
+    integer(kind=int64), parameter:: n_max = 2*b_max+1
     integer(kind=int64), parameter:: n_up = sqrt(real(n_max))+1
-    integer(kind=int64), parameter:: i_up = n_up/2
+    integer(kind=int64), parameter:: b_up = n_up/2
 
-    integer(kind=int64):: i, j
-    logical, dimension(i_max):: is_prime
+    integer(kind=int64):: i, j, b
+    integer(kind=int64), dimension(i_max):: is_prime
 
-    ! is_prime(i) contains whether 2*i+1 is a prime
-    ! number or not.
+    ! the b-th bit (0-63) of is_prime(i) contains whether
+    ! 2*(64*(i-1)+b)+3 is a prime number or not.
     ! The sieve is done on odd numbers from 3 to
-    ! 2*i_max+1.
+    ! 2*b_max+1.
 
-    is_prime(:) = .true.
-    do i = 1, i_up
-        if (.not.is_prime(i)) cycle
-        do j = ((2*i+1)**2-1)/2, i_max, 2*i+1
-            is_prime(j) = .false.
+    do b = 0, 63
+        is_prime(1) = ibset(is_prime(1), b)
+    enddo
+    is_prime(2:i_max) = is_prime(1)
+
+    do b = 1, b_up
+        if (.not. btest(is_prime(1+(b-1)/64), mod(b-1, 64))) cycle
+        do j = ((2*b+1)**2-1)/2, b_max, 2*b+1
+            i = 1 + (j-1)/64
+            is_prime(i) = ibclr(is_prime(i), mod(j-1, 64))
         end do
     end do
 
     open(unit=11, file='sieve.out')
-    do i = 1, i_max
-        if (is_prime(i)) write(11, '(I10)') 2*i+1
+    do b = 0, b_max-1
+        if (btest(is_prime(1+b/64), mod(b, 64))) write(11, '(I10)') 2*b+3
     end do
     close(11)
 end program sieve
